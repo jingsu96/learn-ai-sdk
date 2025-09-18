b1824147f76803966a8c923ae92e1f0f1a77ef78
JingHuang Su <jing.tech.tw@gmail.com>
2025-09-18 22:32:52 +0100

    feat 03: no glue code

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,41 @@
 import * as dotenvSafe from "dotenv-safe";
 import { google } from "@ai-sdk/google";
-import { generateText, streamText } from "ai";
+import { deepseek } from "@ai-sdk/deepseek";
+import { generateText, streamText, type LanguageModel } from "ai";
 
 dotenvSafe.config();
 
-const model = google("gemini-2.0-flash-001");
+const gemini = google("gemini-2.0-flash-001");
+const deepseekMode = deepseek.chat("deepseek-chat");
 
-const { textStream } = streamText({
+const jsExpert = async ({
+  prompt,
   model,
-  prompt: "What is ECMAScript?",
-  // Sometimes the AI has to follow a specific behavior, no matter the prompt it receives. -- use system, it can be use both generateText and streamText
-  system:
-    "You are a JavaScript expert. Please answer the user's question concisely.",
-});
+}: {
+  prompt: string;
+  model: LanguageModel;
+}) => {
+  const { textStream } = streamText({
+    model,
+    prompt,
+    // Sometimes the AI has to follow a specific behavior, no matter the prompt it receives. -- use system, it can be use both generateText and streamText
+    system:
+      "You are a JavaScript expert. Please answer the user's question concisely.",
+  });
 
-for await (let text of textStream) {
-  process.stdout.write(text);
-}
+  for await (let text of textStream) {
+    process.stdout.write(text);
+  }
+};
+
+console.log(`\n===== GEMINI =====\n`);
+
+await jsExpert({ prompt: "What's JavaScript?", model: gemini });
+
+console.log("\n=======================\n");
+
+console.log(`\n===== DEEPSEEK =====\n`);
+
+await jsExpert({ prompt: "What's JavaScript?", model: deepseekMode });
+
+console.log("\n=======================\n");
